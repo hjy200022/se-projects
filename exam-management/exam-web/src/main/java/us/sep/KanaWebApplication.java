8a0309e1d1fcc877beaa7372631ba496ba26c687
Coding <862336406@qq.com>
2020-10-05 15:04:08 +0800

    初步建立项目骨架(未调试)

---
+++ exam-management/exam-web/src/main/java/us/sep/KanaWebApplication.java
@@ -0,0 +1,19 @@
+package us.sep;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author kana
+ */
+@SpringBootApplication
+public class KanaWebApplication  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanaWebApplication.class, args);
+    }
+
+
+}
